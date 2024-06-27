c8f98b08c1365f07de33a589a7b86f4de48f083f
0xiam <iamthebestts1@gmail.com>
2024-06-26 21:52:11 -0300

    Update get.lua

--- get.lua
+++ get.lua
@@ -1,16 +1,16 @@
-local WH = return {
+local WH = {
     {HWID = "d38bf40cfa645b3bfea2e543a7be9fc130b39b92f3283ad64402e21619f2708a"},
     {HWID = "94af877ba872020e6459ba118962dccfafaa6e02fc120722c9a26f4a665e875f"}
   }
-table.freeze(WH)
+
 getgenv().HWID = gethwid()
 function VWH()
     for i, data in ipairs(WH) do 
         if data.HWID == gethwid() then
-            return "TRUE"
+            return true
         else
-            game.Players.LocalPlayer:Kick('Not Whitelisted')
-            return "FALSE"
+            game:Shutdown()
+            return false
         end
     end
 end
